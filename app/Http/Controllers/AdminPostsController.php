d4e06d2ce675a19ccd87b290bb4dd67fe6ddb7f0
yingjhen <s3a632059@student.ncut.edu.tw>
2019-11-26 15:49:26 +0800

    練習6-5 在 PostsController的 update內更新資料

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -34,5 +34,12 @@ class AdminPostsController extends Controller
         Post::create($request->all());
         return redirect()->route('admin.posts.index');
     }
+    public function update(Request$request,$id)
+    {
+        $post=Post::find($id);
+        $post->update($request->all());
+        return redirect()->route('admin.posts.index');
+    }
+
 
 }
